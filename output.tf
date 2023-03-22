6e3dba5a1293859a67f617df079f395f6b40c0d6
r1cebank <siyuangao@pm.me>
2023-03-22 10:47:50 -0700

    Readme and inventory generation

--- output.tf
+++ output.tf
@@ -15,3 +15,21 @@ output "arch_mirror_instances" {
     for k, v in module.arch_mirror : module.arch_mirror[k].label => module.arch_mirror[k]
   }
 }
+
+resource "local_file" "inventory" {
+ filename = "./inventory/hosts.ini"
+ content = <<EOF
+  [k3s-master]
+  %{for i, v in module.k3s_master_cluster}
+  ${module.k3s_master_cluster[i].network[0].ipv4_addresses[0]}
+  %{endfor}
+  [k3s-worker]
+  %{for i, v in module.k3s_worker_cluster}
+  ${module.k3s_worker_cluster[i].network[0].ipv4_addresses[0]}
+  %{endfor}
+  [arch-mirror]
+  %{for i, v in module.arch_mirror}
+  ${module.arch_mirror[i].network[0].ipv4_addresses[0]}
+  %{endfor}
+  EOF
+}
