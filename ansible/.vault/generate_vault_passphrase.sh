0bc9269e94e60537d25e73566da26f2ca48f0650
r1cebank <siyuangao@pm.me>
2024-08-27 22:16:01 -0700

    adding onepassword connect

--- ansible/.vault/generate_vault_passphrase.sh
+++ ansible/.vault/generate_vault_passphrase.sh
@@ -2,5 +2,5 @@
 
 PASSFILE="$(dirname -- "$0")/vault_passphrase.gpg"
 if [ ! -f "$PASSFILE" ]; then
-    openssl rand -hex 32 | head -n1 | gpg --armor --recipient siyuangao@pm.me -e -o $PASSFILE
+    openssl rand -hex 32 | head -n1 | gpg -e -a -r 35CCDC9C649B2C1B -e -o $PASSFILE
 fi
